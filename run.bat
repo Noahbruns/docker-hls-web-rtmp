05cd74b5f7a6059220fd2b49730b222858c25308
Noah Bruns <noah@bawamba.com>
2020-08-31 20:45:38 +0100

    fixes

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-docker run -d -p 1935:1935 -p 80:8080 -v %cd%\nginx.conf:/etc/nginx/nginx.conf -v %cd%\html:/web --restart unless-stopped --name docker-hls-web-rtmp alqutami/rtmp-hls
+docker run -d -p 1935:1935 -p 80:8080 -v "%cd%\nginx.conf":/etc/nginx/nginx.conf -v "%cd%\html":/web --restart unless-stopped --name docker-hls-web-rtmp alqutami/rtmp-hls
 
 PAUSE
